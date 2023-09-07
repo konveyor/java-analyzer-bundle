1f5f8f37f0b1347b679831470bfb7640ea4eccb9
Jason Montleon <jmontleo@redhat.com>
2023-09-07 15:54:32 -0400

    :ghost: Improve maven.default.index gen
    Signed-off-by: Jason Montleon <jmontleo@redhat.com>

--- hack/export.java
+++ hack/export.java
@@ -1,23 +1,44 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.TreeSet;
+import java.util.Iterator;
+import java.util.Set;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.StoredFields;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
-public class Export {
+public class export {
   public static void main(String[] args) throws Exception {
     Path path = Paths.get("index");
     Directory index = FSDirectory.open(path);
+    DirectoryReader dreader = DirectoryReader.open(index);
     IndexReader reader = DirectoryReader.open(index);
+    StoredFields storedFields = reader.storedFields();
+
+    TreeSet<String> groupIds = new TreeSet<String>();
 
     int num = reader.numDocs();
-    for ( int i = 0; i < num; i++)
+    for (int i = 0; i < num; i++)
     {
-        Document d = reader.document( i);
-        System.out.println( "d=" +d);
+        Document d = storedFields.document(i);
+        if (d.get("u") != null) {
+          groupIds.add(d.get("u").split("\\|")[0] + ".*");
+        }
     }
+
+    BufferedWriter out = new BufferedWriter(new FileWriter("maven.default.index"));
+    Iterator it = groupIds.iterator();
+    while(it.hasNext()) {
+      out.write(it.next().toString());
+      out.newLine();
+    }
+
+    out.close();
     reader.close();
   }
 }
