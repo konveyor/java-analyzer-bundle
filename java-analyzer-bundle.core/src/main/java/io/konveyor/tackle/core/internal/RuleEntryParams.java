03ace7a175192556d520de7e570ad96241132fd8
Pranav Gaikwad <pgaikwad@redhat.com>
2024-05-20 19:20:19 -0400

    :sparkles: add ability to set search scope by file paths (#94)
    * add ability to filter search scope by file paths
    
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>
    
    * fix for multi-project workspaces
    
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>
    
    ---------
    
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>

--- java-analyzer-bundle.core/src/main/java/io/konveyor/tackle/core/internal/RuleEntryParams.java
+++ java-analyzer-bundle.core/src/main/java/io/konveyor/tackle/core/internal/RuleEntryParams.java
@@ -2,25 +2,29 @@ package io.konveyor.tackle.core.internal;
 
 import static java.lang.String.format;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class RuleEntryParams {
-    
+
     private final String projectName;
     private final String query;
     private final int location;
     private final String analysisMode;
+    private final ArrayList<String> includedPaths;
 
     public RuleEntryParams(final String commandId, final List<Object> arguments) {
         @SuppressWarnings("unchecked")
         Map<String, Object> obj = (Map<String, Object>) arguments.stream().findFirst()
-                .orElseThrow(() -> new UnsupportedOperationException(format("Command '%s' must be called with one rule entry argument!", commandId)));
+                .orElseThrow(() -> new UnsupportedOperationException(
+                        format("Command '%s' must be called with one rule entry argument!", commandId)));
 
         this.projectName = (String) obj.get("project");
         this.query = (String) obj.get("query");
         this.location = Integer.parseInt((String) obj.get("location"));
         this.analysisMode = (String) obj.get("analysisMode");
+        this.includedPaths = (ArrayList<String>) obj.get("includedPaths");
     }
 
     public String getProjectName() {
@@ -39,4 +43,7 @@ public class RuleEntryParams {
         return analysisMode;
     }
 
+    public ArrayList<String> getIncludedPaths() {
+        return includedPaths;
+    }
 }
