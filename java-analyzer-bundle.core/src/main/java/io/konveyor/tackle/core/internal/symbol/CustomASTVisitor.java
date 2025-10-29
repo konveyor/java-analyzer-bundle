8097e846e70256d52de32fc836a81809a68c8765
Shawn Hurley <shawn@hurley.page>
2025-10-29 13:59:13 -0400

    claude fixed itself
    Signed-off-by: Shawn Hurley <shawn@hurley.page>

--- java-analyzer-bundle.core/src/main/java/io/konveyor/tackle/core/internal/symbol/CustomASTVisitor.java
+++ java-analyzer-bundle.core/src/main/java/io/konveyor/tackle/core/internal/symbol/CustomASTVisitor.java
@@ -202,6 +202,10 @@ public class CustomASTVisitor extends ASTVisitor {
                 // get fqn of the method being called
                 ITypeBinding declaringClass = binding.getDeclaringClass();
                 if (declaringClass != null) {
+                    // Handle Erasure results - strip type parameters for matching
+                    if (declaringClass.getErasure() != null) {
+                        declaringClass = declaringClass.getErasure();
+                    }
                     String fullyQualifiedName = declaringClass.getQualifiedName();
                     // match fqn with query pattern
                     // Note: We keep using regex matching for now as SearchPattern doesn't expose
@@ -226,7 +230,7 @@ public class CustomASTVisitor extends ASTVisitor {
             // this is so that we fallback and don't lose a match when we fail
             this.symbolMatches = true;
             return false;
-        }    
+        }
     }
 
     /* 
@@ -246,6 +250,10 @@ public class CustomASTVisitor extends ASTVisitor {
                 // get fqn of the method being called
                 ITypeBinding declaringClass = binding.getDeclaringClass();
                 if (declaringClass != null) {
+                    // Handle Erasure results - strip type parameters for matching
+                    if (declaringClass.getErasure() != null) {
+                        declaringClass = declaringClass.getErasure();
+                    }
                     String fullyQualifiedName = declaringClass.getQualifiedName();
                     // match fqn with query pattern
                     // Note: We keep using regex matching for now as SearchPattern doesn't expose
@@ -270,7 +278,7 @@ public class CustomASTVisitor extends ASTVisitor {
             // this is so that we fallback and don't lose a match when we fail
             this.symbolMatches = true;
             return false;
-        }    
+        }
     }
 
     public boolean symbolMatches() {
